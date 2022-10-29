291dc9c966c1ca75ac5709203ae928095b22fe06
Knob <59198478+nbr41to@users.noreply.github.com>
2022-10-29 20:36:30 +0900

    .

---
+++ app/about/layout.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <html>
+      <header>About Layout..</header>
+      <nav>
+        <Link href='/'>Home</Link>
+        <Link href='/about'>About</Link>
+        <Link href='/news'>News</Link>
+        <Link href='/news/123'>News/123</Link>
+      </nav>
+      <body>{children}</body>
+    </html>
+  );
+}
